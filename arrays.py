83a83f7c3464783bb8dca058f28ab30e274122ec
Bonifacemusyoki <124658111+Bonifacemusyoki@users.noreply.github.com>
2023-02-14 00:19:05 -0800

    lesson3

---
+++ arrays.py
@@ -0,0 +1,40 @@
+numbers=[9,1,2,5]
+x=numbers[0]
+print(x)
+
+
+numbers=[10,20,50,6]
+numbers[2]=9
+print(numbers)
+
+cars=["bmw","jeep","forester"]
+cars.append("toyota")
+print(cars)
+cars.remove("bmw")
+print(cars)
+
+
+numbers=[3,5,6,77,8,9]
+length=len(numbers)
+print(length)
+
+numbers=[9,11,12,10]
+numbers.pop(2)
+print(numbers)
+
+
+numbers=[1,2,3,4,4,4,7,5]
+length=numbers.count(4)
+print(length)
+numbers.reverse()
+print(numbers)
+for i in numbers:
+    print(i)
+
+for i in range(3):
+    print (numbers[i])
+
+
+
+
+
---
+++ strings.py
@@ -0,0 +1,60 @@
+name="climate"
+print(name)
+
+
+# index
+name[0]
+print(name[0])
+
+
+# length
+print(len(name))
+
+
+# last letter
+name[-1]
+print(name[-1])
+
+
+# printing all the letters
+for letter in name:
+    print(letter)
+
+
+# Joining strings
+name1="steve"
+name2="mbugua"
+print(name1+name2)
+
+name=input("Enter name:")
+print("hello "+ name)
+
+# upper method
+name="boniface"
+print(name.upper())
+
+# checking.....for example if something is true or false
+x="ford"
+print(x.isalpha())
+
+password="stringed4"
+if password.isalpha():
+    print("invalid password")
+else:
+    print("valid password")
+
+
+name=input("Enter username:")
+print("Hello "+ name)
+
+
+name="x123edwin5779"
+
+print(name[4:9])
+
+
+
+
+
+
+
