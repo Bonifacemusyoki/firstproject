13816e24cba4335844195923ee2c196564b7be3d
Bonifacemusyoki <124658111+Bonifacemusyoki@users.noreply.github.com>
2023-02-15 00:40:59 -0800

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+numberl = eval(input("Enter first number:"))
+number2 = eval(input("Enter secund number:"))
+operator = input("Enter operator:")
+
+def add(num1,num2):
+    result=num1 + num2
+    return result
+
+def subtract(num1,num2):
+    result=num1 - num2
+    return result
+
+
+def divide(num1,num2):
+    result=num1 / num2
+    return result
+
+
+def multiply(num1,num2):
+    result=num1 * num2
+    return result
